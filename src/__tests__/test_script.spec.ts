9f6b9ef89d06d389ffc84fada68fd68c8cb068a8
Nguyen Cong Thanh <no7kai@gmail.com>
2023-10-16 15:03:39 +0700

    initial commit

---
+++ src/__tests__/test_script.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
+ */
+
+describe('test_script', () => {
+  it('should be tested', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
